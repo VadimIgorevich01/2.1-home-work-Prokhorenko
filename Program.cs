86502a105768dacce72aac796a9610f97f6ca9f3
Vadim <01prosto01@mail.ru>
2023-05-24 16:12:06 +1000

    1 done

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//Task1_______________________________
+Console.WriteLine("Type 3 digits integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+number = number / 10;
+number = number % 10;
+Console.WriteLine($"Middle digit is {number}");
+//____________________________________
